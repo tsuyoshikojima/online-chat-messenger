bf272c0a5ef58aaf9dfb756206671c342ba06318
tsuyoshi <tkojima.dev@gmail.com>
2026-08-01 11:22:08 +0900

    Add concurrent message receiving to clinet

--- stage1/client.py
+++ stage1/client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 from protocol import encode_message, decode_message
 
@@ -6,6 +7,30 @@ from protocol import encode_message, decode_message
 SERVER_ADDRESS = ("127.0.0.1", 9001)
 
 BUFFER_SIZE = 4096
+
+
+def receive_messages(client_socket: socket.socket) -> None:
+    """データの受信処理と表示をする"""
+
+    while True:
+        try:
+            received_bytes, _ = client_socket.recvfrom(BUFFER_SIZE)
+
+            received_username, received_message = decode_message(received_bytes)
+
+            print(
+                "\n\nメッセージを受信しました。\n"
+                f"ユーザー: {received_username}\n"
+                f"内容: {received_message}\n"  
+            )
+
+        except (ValueError, UnicodeDecodeError) as error:
+            print(f"不正なデータを受信しました: {error}")
+
+        except OSError:
+            # ソケットが閉じられて受信できなくなった場合
+            break
+
 
 try:
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client_socket:
@@ -21,20 +46,16 @@ try:
 
             break
 
+        receive_thread = threading.Thread(target=receive_messages, args=(client_socket,), daemon=True)
+        receive_thread.start()
+
+        print("メッセージを入力してください。")
+
         while True:
-            sending_message = input("メッセージを入力してください。\n> ")
+            sending_message = input("> ")
 
             sending_bytes = encode_message(username, sending_message)
             client_socket.sendto(sending_bytes, SERVER_ADDRESS)
-
-            received_bytes, _ = client_socket.recvfrom(BUFFER_SIZE)
-            received_username, received_message = decode_message(received_bytes)
-
-            print(
-                "メッセージを受信しました。\n"
-                f"ユーザー: {received_username}\n"
-                f"内容: {received_message}"  
-            )
 
 except KeyboardInterrupt:
     print("\nチャットを終了します。")
